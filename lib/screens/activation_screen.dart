daf62ce51e3c5a3064b1701a8bd980f44a55b24b
cvn3443-ctrl <cvn3443@gmail.com>
2026-06-05 13:44:08 +0300

    Create activation_screen.dart

---
+++ lib/screens/activation_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ActivationScreen extends StatelessWidget {
+  const ActivationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('شاشة التفعيل - قيد التطوير'),
+      ),
+    );
+  }
+}
