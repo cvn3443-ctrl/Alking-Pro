97cc3545ad2867667badef43cdfdd8ec67babafb
cvn3443-ctrl <cvn3443@gmail.com>
2026-06-05 17:33:16 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'screens/activation_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  // طلب أذن الإنترنت عند بدء التشغيل
-  await Permission.internet.request();
-  
+void main() {
   runApp(const AlkingProApp());
 }
 
