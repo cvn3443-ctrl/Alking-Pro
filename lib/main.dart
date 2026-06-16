96d99201d7606295150c30ba0fc662463c3e93fc
cvn3443-ctrl <cvn3443@gmail.com>
2026-06-16 17:25:09 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'package:provider/provider.dart';
+import 'providers/trading_provider.dart';
+import 'screens/login_screen.dart';
 
 void main() {
   runApp(const AlkingProApp());
@@ -10,11 +12,14 @@ class AlkingProApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Alking Pro',
-      theme: ThemeData.dark(),
-      home: const HomeScreen(),
-      debugShowCheckedModeBanner: false,
+    return ChangeNotifierProvider(
+      create: (context) => TradingProvider(),
+      child: MaterialApp(
+        title: 'Alking Pro',
+        theme: ThemeData.dark(),
+        home: const LoginScreen(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
